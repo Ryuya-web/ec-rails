ec3dda4a287d3a24d521a9e4fea3967d85d1f428
github Ryuya-web <mofa196@mbox.re>
2021-01-08 09:04:50 +0900

    Update order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,16 +4,15 @@ class Public::OrdersController < ApplicationController
         @cart_items = current_end_user.cart_items
     end
     def confirm
+        @order = Order.new(order_params)
         @cart_items = current_end_user.cart_items
         if  params[:order][:address_option] == "0"
-           @order=Order.new(order_params)
            @order.end_user_id=current_end_user.id
            @order.shipping_name = params[:shipping_name]
            @order.shipping_address = params[:shipping_address]
            @order.shipping_number = params[:shipping_number]
            @order.shipping_price = 800
         elsif params[:order][:address_option] == "1"
-            @order=Order.new(order_params)
             @order.end_user_id=current_end_user.id
             @address = Address.find_by(post_code:params[:address_id])
             @order.shipping_address = @address.street_address
@@ -21,7 +20,6 @@ class Public::OrdersController < ApplicationController
             @order.shipping_price = 800  
             @order.shipping_name = @address.name
         elsif params[:order][:address_option] == "2"
-            @order=Order.new(order_params)
             @order.end_user_id=current_end_user.id
             @order.shipping_price = 800
         elsif
@@ -30,19 +28,19 @@ class Public::OrdersController < ApplicationController
 
     end
     def create
-       @order = Order.new(order_params)
-       @order.end_user_id = current_end_user.id
-       @order.save
+        @order = Order.new(order_params)
+        @order.end_user_id = current_end_user.id
+        @order.save
         cart_items = current_end_user.cart_items
 		cart_items.each do |cart_item|
-			@order_detail = OrderDetail.new
-			@order_detail.order_id = @order.id
-			@order_detail.item_id = cart_item.item.id
-			@order_detail.quantity = cart_item.amount
-			@order_detail.make_status = 0
-			@order_detail.include_price = (cart_item.item.tax_excluded_price * 1.1).floor
-			@order_detail.save
-		end
+            @order_detail = OrderDetail.new
+            @order_detail.order_id = @order.id
+            @order_detail.item_id = cart_item.item_id
+            @order_detail.quantity = cart_item.amount
+            @order_detail.include_price = (cart_item.item.tax_excluded_price * 1.1).floor
+            @order_detail.save
+        end
+       
 		cart_items.destroy_all
         redirect_to order_finish_path
     end
@@ -50,6 +48,6 @@ class Public::OrdersController < ApplicationController
     end
     private
     def order_params
-        params.require(:order).permit(:billing_price,:payment_method,:shipping_address,:shipping_name,:shipping_number)
+        params.require(:order).permit(:billing_price,:payment_method,:shipping_address,:shipping_name,:shipping_number,:shipping_price)
     end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,7 @@
 class OrderDetail < ApplicationRecord
     belongs_to :order
     belongs_to :item
-    validates :quanitity, presence: true
-    validates :inclede_price, presence: true
+    validates :quantity, presence: true
+    validates :include_price, presence: true
     enum make_status: { '着手負荷': 0, '製作待ち': 1, '製作中': 2,'製作完了': 3}
 end
